709d74e6a8084104a1efb3a7ee4b78c302097fe6
farside1969 <farside1969@hotmail.com>
2019-08-26 16:39:33 -0500

    Clarified comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,27 +6,30 @@ public class Main {
 // method
     public static void main(String[] args) {
 
-// declare input integer - maximum number to check up to
-        int num = 100;
+// declare input integer(s)
+        // integer to run up to
+        int num = 175;
+        // first divisible integer
         int div1 = 5;
+        //second divisible integer
         int div2 = 7;
 
-// loop forward starting at integer 1
+// loop forward (positive) starting at integer 1
         for (int i = 1; i <= num; i++) {
 
-// set both of the multiple of integers
+// if divisible by both div1 and div2 integers (use modulus - remainder is 0)
             if (((i % div1) == 0) & ((i % div2) == 0))
                 System.out.println("fizzbuzz");
 
-// is a multiple of lowest integer
+// if divisible by div1 integer (use modulus - remainder is 0)
             else if ((i % div1) == 0)
                 System.out.println("fizz");
 
-// is a multiple of th higher integer
+// if divisible by div2 integer (use modulus - remainder is 0)
             else if ((i % div2) == 0)
                 System.out.println("buzz");
 
-// is not a multiple of either integer
+// is not a multiple of either div1 or div2
             else
                 System.out.println(i);
             }
