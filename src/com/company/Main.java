d68a484e4e02b547a28de6ccab1f7e00fd567f1a
farside1969 <farside1969@hotmail.com>
2019-08-26 16:31:19 -0500

    added variable fixed input div1 & div2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,20 +8,22 @@ public class Main {
 
 // declare input integer - maximum number to check up to
         int num = 100;
+        int div1 = 5;
+        int div2 = 7;
 
 // loop forward starting at integer 1
         for (int i = 1; i <= num; i++) {
 
 // set both of the multiple of integers
-            if (((i % 5) == 0) & ((i % 7) == 0))
+            if (((i % div1) == 0) & ((i % div2) == 0))
                 System.out.println("fizzbuzz");
 
 // is a multiple of lowest integer
-            else if ((i % 5) == 0)
+            else if ((i % div1) == 0)
                 System.out.println("fizz");
 
 // is a multiple of th higher integer
-            else if ((i % 7) == 0)
+            else if ((i % div2) == 0)
                 System.out.println("buzz");
 
 // is not a multiple of either integer
