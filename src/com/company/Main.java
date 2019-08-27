42da9d5c406abb980b80e68acf96bc9b62200b6b
farside1969 <farside1969@hotmail.com>
2019-08-27 15:33:40 -0500

    .gitignore added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,26 +10,26 @@ public class Main {
         // integer to run up to
         int num = 175;
         // first divisible integer
-        int div1 = 5;
+        int div1 = 3;
         //second divisible integer
-        int div2 = 7;
+        int div2 = 5;
 
 // loop forward (positive) starting at integer 1
         for (int i = 1; i <= num; i++) {
 
-// if divisible by both div1 and div2 integers (use modulus - remainder is 0)
+// fizzbuzz if divisible by both div1 and div2 integers (use  - remainder is 0)
             if (((i % div1) == 0) & ((i % div2) == 0))
                 System.out.println("fizzbuzz");
 
-// if divisible by div1 integer (use modulus - remainder is 0)
+// fizz if divisible by div1 integer "fizz" (use modulus - remainder is 0)
             else if ((i % div1) == 0)
                 System.out.println("fizz");
 
-// if divisible by div2 integer (use modulus - remainder is 0)
+// buzz if divisible by div2 integer "buzz" (use modulus - remainder is 0)
             else if ((i % div2) == 0)
                 System.out.println("buzz");
 
-// is not a multiple of either div1 or div2
+// integer if not a multiple of either div1 or div2
             else
                 System.out.println(i);
             }
